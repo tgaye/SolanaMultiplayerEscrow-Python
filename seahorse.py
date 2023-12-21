53d6dae31e45439a035d11681f38a3736a6623c6
scaredofboobs <tylercgaye@gmail.com>
2023-12-21 10:24:57 -0700

    Update seahorse.py

--- seahorse.py
+++ seahorse.py
@@ -29,35 +29,40 @@ class GameGear(Program):
         self.max_lobbies = 10
 
     def deposit(self, ctx, player: Player, amount: u64):
-        # SPL token transfer logic
+        # Overflow check
+        assert player.balance + amount > player.balance, "Overflow detected"
         Token.transfer(ctx, amount, ctx.accounts.player_token_account, ctx.accounts.platform_token_account)
         player.balance += amount
 
     def withdraw(self, ctx, player: Player, amount: u64):
+        # Underflow check
         assert player.balance >= amount, "Insufficient funds"
-        # SPL token transfer logic
         Token.transfer(ctx, amount, ctx.accounts.platform_token_account, ctx.accounts.player_token_account)
         player.balance -= amount
 
     def withdraw_fees(self, ctx, owner: Signer):
         fees = self.platform_fees
         self.platform_fees = 0
-        # SPL token transfer logic
+        # Overflow check not needed for zeroing a value
         Token.transfer(ctx, fees, ctx.accounts.platform_token_account, ctx.accounts.owner_token_account)
 
     def create_game(self, ctx, owner: Signer, player1: PublicKey, player2: PublicKey, wager: u64):
         fee = wager // 20
         total_deduction = wager + fee
-        # Check balances and deduct from players
         p1 = ctx.accounts.get(player1)
         p2 = ctx.accounts.get(player2)
+        # Underflow checks
         assert p1.balance >= total_deduction and p2.balance >= total_deduction, "Insufficient funds for one of the players"
         
         p1.balance -= total_deduction
         p2.balance -= total_deduction
+        # Overflow check for platform fees
+        assert self.platform_fees + (fee * 2) > self.platform_fees, "Overflow detected"
         self.platform_fees += fee * 2
 
         game_id = self.game_counter
+        # Overflow check for game counter
+        assert game_id + 1 > game_id, "Overflow detected"
         self.game_counter += 1
         ctx.accounts.games[game_id] = Game({
             player1: player1,
@@ -76,6 +81,13 @@ class GameGear(Program):
         winner_account = ctx.accounts.get(winner)
         loser = game.player1 if game.player2 == winner else game.player2
         loser_account = ctx.accounts.get(loser)
+
+        # Overflow checks for balance, total won, wins, total lost, and losses
+        assert winner_account.balance + winner_prize > winner_account.balance, "Overflow detected"
+        assert winner_account.total_won + game.wager > winner_account.total_won, "Overflow detected"
+        assert winner_account.wins + 1 > winner_account.wins, "Overflow detected"
+        assert loser_account.total_lost + game.wager > loser_account.total_lost, "Overflow detected"
+        assert loser_account.losses + 1 > loser_account.losses, "Overflow detected"
 
         winner_account.balance += winner_prize
         winner_account.total_won += game.wager
